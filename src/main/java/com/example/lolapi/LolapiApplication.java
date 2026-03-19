ce71996fb44d08a55c2fd574344cf3e1064a9aff
CBatista <cabatist@uncg.edu>
2026-03-19 05:36:26 -0400

    Move source files to correct package structure

--- src/main/java/com/example/lolapi/LolapiApplication.java
+++ src/main/java/com/example/lolapi/LolapiApplication.java
@@ -2,11 +2,8 @@ package com.example.lolapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
- 
+
 @SpringBootApplication
-@RestController
 
 public class LolapiApplication {
     public static void main(String[] args) {
